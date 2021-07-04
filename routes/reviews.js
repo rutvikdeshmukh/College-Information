4e4865b9ba1157ecf66a23d02c89c1625fdbe8a8
rutvikdeshmukh <rutvikdeshmukh901@gmail.com>
2021-07-04 16:17:18 +0530

    data

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,6 +3,7 @@ const router = express.Router({ mergeParams: true });
 const catchAsync = require("../utils/catchAsync");
 const { authentication, reviewValidation } = require("../middleware");
 const reviews = require("../controllers/reviews");
+const { checkReviewAuthor } = require("../middleware");
 
 router.post(
   "/",
@@ -13,12 +14,19 @@ router.post(
 router.delete(
   "/:review_id/delete",
   authentication,
+  checkReviewAuthor,
   catchAsync(reviews.delete_review)
 );
-router.get("/:review_id/edit", authentication, catchAsync(reviews.edit_review));
+router.get(
+  "/:review_id/edit",
+  authentication,
+  checkReviewAuthor,
+  catchAsync(reviews.edit_review)
+);
 router.patch(
   "/:review_id/update",
   authentication,
+  checkReviewAuthor,
   reviewValidation,
   catchAsync(reviews.update_review)
 );
