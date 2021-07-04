6fa86cd783bcc1243fa471b396727115d30a5039
rutvikdeshmukh <rutvikdeshmukh901@gmail.com>
2021-07-04 13:22:24 +0530

    data

--- controllers/college.js
+++ controllers/college.js
@@ -53,7 +53,6 @@ module.exports.edit_college = async (req, res, next) => {
   return res.render("student/editForm.ejs", { record });
 };
 module.exports.update_college = async (req, res, next) => {
-  let size;
   const { id } = req.params;
   const geodata = await geocoder
     .forwardGeocode({
@@ -70,12 +69,9 @@ module.exports.update_college = async (req, res, next) => {
         filename: element.filename,
       }));
       record.image.push(...image);
-      size = record.image.length;
     }
-    size = record.image.length;
-    await record.save();
     if (req.body.deleteImages) {
-      if (req.body.deleteImages.length === size) {
+      if (req.body.deleteImages.length === record.image.length) {
         return next(new ExpressError("Minimum One college image is required"));
       }
       for (let filename of req.body.deleteImages) {
@@ -89,7 +85,7 @@ module.exports.update_college = async (req, res, next) => {
         { new: true }
       );
     }
-
+    await record.save();
     req.flash("success", "Information Updated Successfully");
     return res.redirect(`/college/${id}`);
   } else {
