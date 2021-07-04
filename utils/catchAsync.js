6f183b6e7662e79bf9be92fdd3a66acc0f4bc238
rutvikdeshmukh <rutvikdeshmukh901@gmail.com>
2021-07-04 18:42:01 +0530

    data

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,8 +1,10 @@
 const catchAsync = function (fn) {
   return async function (req, res, next) {
-    fn(req, res, next).catch((e) => {
+    try {
+      fn(req, res, next);
+    } catch (e) {
       return next(e);
-    });
+    }
   };
 };
 
